04ed73b51c609c7daf65f7e11f290fc78bc52939
ramcmd <sriram.ananthakrishna@gmail.com>
2022-07-20 02:07:00 -0400

    initial commit

---
+++ Min Stack.py
@@ -0,0 +1,35 @@
+# TC: O(1) 
+# SC: O(n)
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minstack:
+            self.minstack.append(val)
+        elif val <= self.minstack[-1]:
+            self.minstack.append(val)
+
+    def pop(self) -> None:
+        ele = self.stack.pop()
+        if ele == self.minstack[-1]:
+            self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
